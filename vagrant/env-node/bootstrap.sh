ffaa778327d0033f33748bb994e6495890b78d1d
Gregorio Galante <gregogalante@gmail.com>
2017-03-17 01:10:42 +0100

    Update bootstrap.sh

--- vagrant/env-node/bootstrap.sh
+++ vagrant/env-node/bootstrap.sh
@@ -8,10 +8,7 @@ sudo apt-get install -y nodejs
 
 # Install mongodb
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org mongodb-org-server
+sudo apt-get install -y mongodb  
 
 # Install yarn for better dependencies management
 
