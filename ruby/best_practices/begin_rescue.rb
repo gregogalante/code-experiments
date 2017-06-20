e9abb5ca35ce77c5d9848e1f76f5660e6f827e2e
Gregorio Galante <gregogalante@gmail.com>
2017-06-20 23:49:51 +0200

    complete begin example

--- ruby/best_practices/begin_rescue.rb
+++ ruby/best_practices/begin_rescue.rb
@@ -7,3 +7,36 @@ begin
 rescue => e
   puts e # -> undefined local variable or method `undefined_variable' for main:Object
 end
+
+puts 'Complete structure:'
+
+begin
+  # something which might raise an exception
+rescue => e
+  # code that deals with some exception
+else
+  # code that runs only if *no* exception was raised
+ensure
+  # ensure that this code always runs, no matter what
+end
+
+begin
+  puts undefined_variable
+rescue => e
+  puts e # -> undefined local variable or method `undefined_variable' for main:Object
+else
+  puts 'good work ;)'
+ensure
+  puts 'i will be executed :)' # -> i will be executed :)
+end
+
+begin
+  defined_variable = 1
+  puts defined_variable # -> 1
+rescue => e
+  puts e
+else
+  puts 'good work ;)' # -> good work ;)
+ensure
+  puts 'i will be executed :)' # -> i will be executed :)
+end
