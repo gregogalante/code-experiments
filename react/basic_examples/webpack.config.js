5612b49fe13c5beb65e8b79747c1063bf890aef9
Gregorio Galante <gregogalante@gmail.com>
2017-10-07 15:21:38 +0200

    add example of react

---
+++ react/basic_examples/webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const BUILD_DIR = path.resolve(__dirname, 'build')
+const APP_DIR = path.resolve(__dirname, 'src')
+
+const config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
+
+module.exports = config
