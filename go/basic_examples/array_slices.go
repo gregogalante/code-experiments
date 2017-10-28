9ab3734f3c91d128abc39aabbd111e01b3767269
Gregorio Galante <gregogalante@gmail.com>
2017-10-28 17:21:55 +0200

    update slices

--- go/basic_examples/array_slices.go
+++ go/basic_examples/array_slices.go
@@ -10,6 +10,19 @@ This selects a half-open range which includes the first element, but excludes th
 
 A slice does not store any data, it just describes a section of an underlying array.
 Changing the elements of a slice modifies the corresponding elements of its underlying array.
+
+The length of a slice is the number of elements it contains.
+The capacity of a slice is the number of elements in the underlying array, counting from
+the first element in the slice.
+
+A slice has both a length and a capacity.
+The length of a slice is the number of elements it contains.
+The capacity of a slice is the number of elements in the underlying array, counting from the
+first element in the slice.
+
+Slices can be created with the built-in make function; this is how you create dynamically-sized arrays.
+
+Slices can contain any type, including other slices.
 */
 
 package main
@@ -17,8 +30,19 @@ package main
 import "fmt"
 
 func main() {
+	// Small slice example
 	var primes = [6]int{2, 3, 5, 7, 11, 13}
-
 	var s = primes[1:4]
 	fmt.Println(s)
+
+	// Slice default value
+	var a [4]int
+	fmt.Println(a[0:4])
+	fmt.Println(a[:4])
+	fmt.Println(a[0:])
+	fmt.Println(a[:])
+
+	// Create slice with make
+	var c = make([]int, 5)    // len(a)=5
+	var d = make([]int, 0, 5) // len(b)=0, cap(b)=5
 }
