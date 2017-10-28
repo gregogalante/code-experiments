dfeb621b48d3daa917c5a5c11b677d256a3aa497
Gregorio Galante <gregogalante@gmail.com>
2017-10-28 15:27:37 +0200

    add struct example

---
+++ go/basic_examples/structs.go
@@ -0,0 +1,18 @@
+/*
+A struct is a collection of fields.
+*/
+
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	var v = Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
+}
