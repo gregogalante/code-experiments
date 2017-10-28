62e6b45943459bbeb844a4d285cb844b86ce8d43
Gregorio Galante <gregogalante@gmail.com>
2017-10-28 14:31:30 +0200

    add defer examples

---
+++ go/basic_examples/defer.go
@@ -0,0 +1,19 @@
+/*
+A defer statement defers the execution of a function until the surrounding function returns.
+The deferred call's arguments are evaluated immediately, but the function call
+is not executed until the surrounding function returns.
+*/
+
+package main
+
+import "fmt"
+
+func beautiful() {
+	fmt.Println("beautiful")
+}
+
+func main() {
+	defer fmt.Println("world")
+	fmt.Println("hello")
+	beautiful()
+}
