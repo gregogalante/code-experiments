c0e885027e8f3783d0ea99e31a69c8524d83f1b5
Gregorio Galante <gregogalante@gmail.com>
2017-11-02 16:28:13 +0100

    fix serve static files server

--- go/microservices_course/file_server/file_server.go
+++ go/microservices_course/file_server/file_server.go
@@ -11,7 +11,7 @@ func main() {
 	log.Printf("Server starting on port %v\n", port)
 
 	// call the Handle method to create an handler for a filesystem server
-	http.Handle("/images", http.FileServer(http.Dir("./images")))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
 
 	// start the HTTP server
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
