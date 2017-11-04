135e65e3bd682fd7746ce36f5f50ee21cf0b48e1
Gregorio Galante <gregogalante@gmail.com>
2017-11-04 10:58:04 +0100

    update web server

--- go/web_server/web_server.go
+++ go/web_server/web_server.go
@@ -3,20 +3,21 @@ package main
 import (
 	"fmt"
 	"log"
+	"io"
 	"net/http"
 )
 
-func main() {
-	port := 8080
-	log.Printf("Server starting on port %v\n", port)
-
-	// call the HandleFunc method which creates a Handler type
-	http.HandleFunc("/helloworld", helloWorldHandler)
-
-	// start the HTTP server
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+func home(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
 }
 
-func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World\n")
+func main() {
+	port := 8080
+	portString := fmt.Sprintf(":%v", port)
+
+	log.Printf("Server starting on port %v\n", port)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	http.ListenAndServe(portString, mux)
 }
