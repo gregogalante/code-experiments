280000a3e4c9a2c95985045b1a70085f8e434a2b
Gregorio Galante <gregogalante@gmail.com>
2017-11-25 14:57:20 +0100

    init react reconciler

---
+++ javascript/react-reconciler/lib/index.js
@@ -0,0 +1,17 @@
+const Reconciler = require('react-reconciler')
+
+const ReconcilerConfig = {
+  // You'll need to implement some methods here.
+  // See below for more information and examples.
+}
+
+var MyRenderer = Reconciler(ReconcilerConfig)
+
+var RendererPublicAPI = {
+  render(element, container, callback) {
+    // Call MyRenderer.updateContainer() to schedule changes on the roots.
+    // See ReactDOM, React Native, or React ART for practical examples.
+  }
+}
+
+module.exports = RendererPublicAPI
